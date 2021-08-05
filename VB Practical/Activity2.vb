c8fd15e82666c55b2b8772d15a95bc2c9fda15b1
IocaneDevApps <61220404+IocaneDevApps@users.noreply.github.com>
2021-08-05 13:35:48 +0930

    Update Activity2.vb
    Added 2.3.2 Coding Activities

--- VB Practical/Activity2.vb
+++ VB Practical/Activity2.vb
@@ -1,7 +1,18 @@
 Imports System
 
 Module Activity2
+
     Sub Main(args As String())
+
+        FindAndFixFault()
+
+        'Coding()
+
+    End Sub
+
+
+    ' 2.3.1 Find and Fix Fault
+    Sub FindAndFixFault()
 
         'The loops below should print out the first 88 numbers in octal (base 8). It has a fault in the code somewhere, find and fix the problem.
         'Example of first few numbers in octal:
@@ -13,5 +24,49 @@ Module Activity2
             Next units
         Next base8
 
+        Console.WriteLine("End FindAndFixFault()")
+        Console.ReadLine()
+
     End Sub
+
+
+
+    '2.3.2 Coding
+    Sub Coding()
+
+        Dim cells As New Dictionary(Of String, Object)()
+
+        For i As Integer = 1 To 10
+            cells(String.Format("A:{0}", i)) = String.Format("Order #{0}", i)
+            cells(String.Format("B:{0}", i)) = DateTime.Now.ToString
+            cells(String.Format("C:{0}", i)) = i * 18
+        Next
+
+        For Each pair As KeyValuePair(Of String, Object) In cells
+            Console.WriteLine(pair)
+        Next
+
+
+        ' 2.3.2.1 Dates
+        ' code solution here 
+
+
+        ' 2.3.2.2 Cell Currency
+        ' code solution here 
+
+
+        ' 2.3.2.3 Switch Cells
+        ' code solution here 
+
+
+        ' 2.3.2.4 Print Table
+        ' code solution here 
+
+
+        Console.WriteLine("End Coding()")
+        Console.ReadLine()
+
+    End Sub
+
+
 End Module
